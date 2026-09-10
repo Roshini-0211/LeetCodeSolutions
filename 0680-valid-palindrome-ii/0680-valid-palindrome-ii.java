6c7f715dc905bdaee81dab7957fb860d8013e9de
Roshini A <156168425+Roshini-0211@users.noreply.github.com>
2026-09-10 21:24:31 +0530

    Time: 4 ms (98.59%), Space: 47.7 MB (68.57%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            // If characters mismatch, try deleting one character
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+    
+    // Helper method to check if a substring is a perfect palindrome
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
